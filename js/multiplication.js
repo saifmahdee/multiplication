f1f41c812fc559fec834a56a1d3c38c12cc3eba3
Saifullah Al Mahdee <saif.mahdee@gmail.com>
2018-01-26 13:57:48 +0600

    add files

---
+++ js/multiplication.js
@@ -0,0 +1,11 @@
+function multiplication() {
+
+	while (let i = 1; let i <= 20; i++) {
+		
+		
+	}
+
+	console.log(i*2);
+}
+
+multiplication();
---
+++ js/two.js
@@ -0,0 +1,10 @@
+var x,y,z;
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    z=z+("*");        
+      }
+ console.log(z);
+ z='';    
+}
